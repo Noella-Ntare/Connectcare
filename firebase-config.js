9d79398d320474d08b734cceba5757464a68ddb4
Noella-Ntare <n.ntare@alustudent.com>
2025-07-30 18:54:41 +0200

    updated app

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+ 
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const firebaseConfig = {
+      apiKey: "AIzaSyBPWWfWXWdOd3WP0we-fFOhnI-zmInRMLo",
+      authDomain: "connectcare-d741a.firebaseapp.com",
+      projectId: "connectcare-d741a",
+      storageBucket: "connectcare-d741a.firebasestorage.app",
+      messagingSenderId: "237656403934",
+      appId: "1:237656403934:web:ac1e24f0c9b8c98c62774f",
+    };
+</script>
+
+
+
+
+
